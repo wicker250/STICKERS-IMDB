552c4be392fa3932a981b3f73b425a30b0ee61bd
wicker250 <afonso.silveira1@hotmail.com>
2022-07-21 22:24:15 -0300

    adicionando texto a figurinha

--- STICKERS/src/StickersMaker.java
+++ STICKERS/src/StickersMaker.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+
+import java.awt.Color;
+import java.awt.Font;
+
 public class StickersMaker {
 
     
@@ -14,7 +16,7 @@ public class StickersMaker {
     public void cria() throws IOException {
         // leitura da imagem
         
-        BufferedImage imagemOriginal = ImageIO.read(new File("C:/Users/Afonso/Desktop/Ws-java-alura/STICKERS/img/filme.jpg"));
+        BufferedImage imagemOriginal = ImageIO.read(new File("C:/Users/Afonso/Desktop/Ws-java-alura/STICKERS/img/filmemaior.jpg"));
 
         
         // cria nova imagem em memória com transparência e com tamanho novo
@@ -26,12 +28,26 @@ public class StickersMaker {
         // nova imagem com fundo transparente "alfa"
         var novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
         // copiar a imagem original para novo imagem (em memória)
-        Graphics2D graphics2D = (Graphics2D) novaImagem.getGraphics();
-        graphics2D.drawImage(imagemOriginal, 0, 0, null);
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
 
 
+        //configurar a fonte
+       int tamanhoFonte = largura/10;
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD,tamanhoFonte);
+        graphics.setFont(fonte);
+        graphics.setColor(Color.RED);
         // escrever uma frase na nova imagem
+        String text ="TOPZERA";
+        int tamText = text.length();
+        //
+        graphics.drawString(text ,(largura)-(tamText*tamanhoFonte),novaAltura - 100);
 
+        System.out.println("largura: "+largura);
+        System.out.println("altura: "+altura);
+        System.out.println(tamText);
+        System.out.println(tamText*tamanhoFonte);
+        System.out.println("tamfonte"+tamanhoFonte);
         // escrever a nova imagem em um arquivo
      
       
@@ -41,5 +57,6 @@ public class StickersMaker {
     public static void main(String[] args) throws IOException {
         StickersMaker gerada = new StickersMaker();
         gerada.cria();
+        
     }
 }
